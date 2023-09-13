e07e7d721ff951b9646d7aa2591553984e8879ab
Dennys Huber <dennys.huber@proton.me>
2023-09-14 00:10:35 +0200

    added gitFull

--- system/configuration.nix
+++ system/configuration.nix
@@ -220,7 +220,7 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    git
+    gitFull
     fprintd
     exa
     fzf
