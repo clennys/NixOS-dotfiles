f59a138785d758a78d70735d89221c48f9165169
Dennys Huber <dennys.huber@proton.me>
2024-02-11 21:40:33 +0100

    removed unnecessary files

--- users/dhuber/.zsh/export.zsh
+++
@@ -1,8 +0,0 @@
-export FZF_DEFAULT_OPTS=$FZF_DEFAULT_OPTS'
---color=dark
---color=fg:#eaebed,bg:-1,hl:#bf616a,fg+:#eaebed,bg+:-1,hl+:#ebcb8b
---color=info:#a3be8c,prompt:#81a1c1,pointer:#bf616a,marker:#ebcb8b,spinner:#96b4b5,header:#81a1c1'
-# export MANPAGER="nvim -c 'set ft=man' -"
-export GOPATH=$HOME/go
-export EDITOR='nvim'
-export ZK_NOTEBOOK_DIR=$HOME/Nextcloud/notebook/
